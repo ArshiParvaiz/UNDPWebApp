3c0d1ebac3489c7213b2b436dfad1d4046851a7e
ArshiParvaiz <47865760+ArshiParvaiz@users.noreply.github.com>
2019-12-08 12:33:19 +0500

    codeedit

---
+++ WebApiDemo/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public double BasePrice {get; set;}
+        public string description{ get; set; }
+    }
+}
